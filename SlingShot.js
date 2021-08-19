fd425d79f77dce96dcf148292aa99316a5462f9b
karthikpnair06 <karthikpnair06@gmail.com>
2021-08-19 16:59:55 +0530

    changes uploaded

--- SlingShot.js
+++ SlingShot.js
@@ -6,18 +6,34 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
         this.point = point;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
     display(){
+        image (this.sling1,210,70,30,150);
+        image (this.sling2,190,55,30,100);
         if (this.sling.bodyA){
             var point1= this.sling.bodyA.position;
             var point2 = this.point;
             push();
-            stroke("black");
+            if(point1.x<220){
+                stroke(48,22,8);
+                strokeWeight(4);
+                line(point1.x-25,point1.y,point2.x-10, point2.y+20);
+                line(point1.x-25,point1.y,point2.x+20,point2.y+30);
+                image(this.sling3,point1.x-25,point1.y-10,15,30);
+            }
+            else{
+                stroke(48,22,8);
             strokeWeight(4);
-            line(point1.x,point1.y,point2.x, point2.y);
+            line(point1.x-25,point1.y,point2.x-10, point2.y+20);
+            line(point1.x-25,point1.y,point2.x+20,point2.y+30);
+            image(this.sling3,point1.x+25,point1.y-10,15,30);
+            }
             pop();
         }
         
--- sketch.js
+++ sketch.js
@@ -38,11 +38,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(210,60);
 
     //log6 = new Log(230, 180, 80, PI/2);
 
-    slingshot = new SlingShot(bird.body, {x: 100, y:100});
+    slingshot = new SlingShot(bird.body, {x: 210, y:60});
 
 }
 
